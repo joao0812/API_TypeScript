fe8e9c79a3f33b76cafece33576df3d25d0cac48
joao0812 <joaoesalmeida@hotmail.com>
2023-09-06 16:16:08 -0300

    fix: implements dotenv to get mongodb atlas url

--- src/config/dbConnection.ts
+++ src/config/dbConnection.ts
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 import { config } from "dotenv";
 config() 
 
-const mongoDB_connection = process.env.mongoDB || ""; 
+const mongoDB_connection = process.env.MONGODB_ATLAS || ""; 
 
 mongoose.connect(mongoDB_connection);
 
